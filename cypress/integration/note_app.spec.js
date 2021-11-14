f0ba53ddd58abacb8e65ea79b0dfb3f3f7029661
Latrell <lll4xy@virginia.edu>
2021-11-14 11:35:54 -0500

    part 5d so far

---
+++ cypress/integration/note_app.spec.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+describe('Note app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2021')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('login').click()
+  })
+
+  it('user can login', function () {
+    cy.contains('login').click()
+    cy.get('#username').type('llee')
+    cy.get('#password').type('test')
+    cy.get('#login-button').click()
+
+    cy.contains('Latrell Lee logged in')
+  })
+})
