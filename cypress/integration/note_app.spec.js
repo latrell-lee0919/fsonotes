0465ec6bce30eb57a95fbd0993530ccf633e1aee
Latrell <lll4xy@virginia.edu>
2021-11-15 17:10:56 -0500

    part 5d updates

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -1,6 +1,13 @@
 /* eslint-disable no-undef */
 describe('Note app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Latrell Lee',
+      username: 'llee',
+      password: 'test'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -20,5 +27,38 @@ describe('Note app', function() {
     cy.get('#login-button').click()
 
     cy.contains('Latrell Lee logged in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('input:first').type('llee')
+      cy.get('input:last').type('test')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exists', function () {
+      beforeEach(function () {
+        cy.contains('new note').click()
+        cy.get('input').type('another note cypress')
+        cy.contains('save').click()
+      })
+
+      it('it can be made important', function () {
+        cy.contains('another note cypress')
+          .contains('make important')
+          .click()
+
+        cy.contains('another note cypress')
+          .contains('make not important')
+      })
+    })
   })
 })
