86678d5af410f504d4a3911b7cd8b79714198c33
Latrell <lll4xy@virginia.edu>
2021-11-17 11:29:32 -0500

    part 5d final

--- cypress/integration/note_app.spec.js
+++ cypress/integration/note_app.spec.js
@@ -10,19 +10,19 @@ describe('Note app', function() {
     cy.visit('http://localhost:3000')
   })
 
-  it.only('login fails with wrong password', function() {
-    cy.contains('login').click()
-    cy.get('#username').type('llee')
-    cy.get('#password').type('wrong')
-    cy.get('#login-button').click()
+  //   it.only('login fails with wrong password', function() {
+  //     cy.contains('login').click()
+  //     cy.get('#username').type('llee')
+  //     cy.get('#password').type('wrong')
+  //     cy.get('#login-button').click()
 
-    cy.get('.error')
-      .should('contain', 'Wrong credentials')
-      .and('have.css', 'color', 'rgb(255, 0, 0)')
-      .and('have.css', 'border-style', 'solid')
+  //     cy.get('.error')
+  //       .should('contain', 'Wrong credentials')
+  //       .and('have.css', 'color', 'rgb(255, 0, 0)')
+  //       .and('have.css', 'border-style', 'solid')
 
-    cy.get('html').should('not.contain', 'Latrell Lee logged in')
-  })
+  //     cy.get('html').should('not.contain', 'Latrell Lee logged in')
+  //   })
 
   it('front page can be opened', function() {
     cy.contains('Notes')
@@ -43,28 +43,42 @@ describe('Note app', function() {
       cy.login({ username: 'llee', password: 'test' })
     })
 
-    it('a new note can be created', function() {
-      cy.contains('new note').click()
-      cy.get('input').type('a note created by cypress')
-      cy.contains('save').click()
-      cy.contains('a note created by cypress')
-    })
+    // it('a new note can be created', function() {
+    //   cy.contains('new note').click()
+    //   cy.get('input').type('a note created by cypress')
+    //   cy.contains('save').click()
+    //   cy.contains('a note created by cypress')
+    // })
 
-    describe('and a note exists', function () {
+    // describe('and a note exists', function () {
+    //   beforeEach(function () {
+    //     cy.createNote({
+    //       content: 'another note cypress',
+    //       important: false
+    //     })
+    //   })
+
+    //   it('it can be made important', function () {
+    //     cy.contains('another note cypress')
+    //       .contains('make important')
+    //       .click()
+
+    //     cy.contains('another note cypress')
+    //       .contains('make not important')
+    //   })
+    // })
+
+    describe('and several notes exist', function () {
       beforeEach(function () {
-        cy.createNote({
-          content: 'another note cypress',
-          important: false
-        })
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
       })
 
-      it('it can be made important', function () {
-        cy.contains('another note cypress')
-          .contains('make important')
-          .click()
-
-        cy.contains('another note cypress')
-          .contains('make not important')
+      it('one of those can be made important', function () {
+        cy.contains('second note').parent().find('button').as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'make not important')
       })
     })
   })
